ecaf38deba890f89b581a438c8ff301269b0d4dc
MrCrayfish <mrcrayfish@hotmail.com>
2016-04-03 14:10:30 +0930

    Updated version info

--- 1.8.9/src/main/java/com/mrcrayfish/furniturece/Reference.java
+++ 1.8.9/src/main/java/com/mrcrayfish/furniturece/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
 	public static final String MOD_ID = "cfmce";
 	public static final String NAME = "MrCrayfish's Furntiure Mod: Community Edition";
-	public static final String VERSION = "1.0.1";
+	public static final String VERSION = "1.1";
 	public static final String CLIENT_PROXY_CLASS = "com.mrcrayfish.furniturece.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "com.mrcrayfish.furniturece.proxy.ServerProxy";
 	public static final String WORKING_MC_VERSION = "[1.8.9]";
